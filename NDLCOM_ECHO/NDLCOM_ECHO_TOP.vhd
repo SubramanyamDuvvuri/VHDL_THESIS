f6554d9a9eba8c936adfb3f284bea6c0a6cab703
Subramanym <duvvurisubramanyam89@gmail.com>
2015-03-27 11:42:55 +0100

    changes for inspection

--- NDLCOM_ECHO/NDLCOM_ECHO_TOP.vhd
+++ NDLCOM_ECHO/NDLCOM_ECHO_TOP.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
+-- Uncomment the following library declaration if ,instantiating
 -- any Xilinx primitives in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
